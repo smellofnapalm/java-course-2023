264961b032d1fb6ccbead895b6534d4bcce213ac
Mikhail <69933358+smellofnapalm@users.noreply.github.com>
2023-10-26 23:13:20 +0300

    Added Main to run the game

---
+++ src/main/java/hangman/Main.java
@@ -0,0 +1,9 @@
+package hangman;
+
+public class Main {
+    public static void main(String[] args) {
+        final int wrongAttemptsAllowed = 5;
+        Hangman game = new Hangman(wrongAttemptsAllowed);
+        game.run();
+    }
+}
