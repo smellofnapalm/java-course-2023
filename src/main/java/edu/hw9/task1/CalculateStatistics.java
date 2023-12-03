5bbf82ee3d2191d7360846bb1c63f3a6e82f6fa8
Mikhail <69933358+smellofnapalm@users.noreply.github.com>
2023-12-03 20:18:57 +0300

    Initial

---
+++ src/main/java/edu/hw9/task1/CalculateStatistics.java
@@ -0,0 +1,4 @@
+package edu.hw9.task1;
+
+public class CalculateStatistics {
+}
---
+++ src/main/java/edu/hw9/task2/FilesFinder.java
@@ -0,0 +1,11 @@
+package edu.hw9.task2;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class FilesFinder {
+    private static final int FILES_IN_DIR = 1000;
+    public static List<Path> findLargeDirs() {
+
+    }
+}
